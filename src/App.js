fb7c699047b294d4bd2a66fd8f03bc1533618c5c
Andrew Odendaal <aoaws@amazon.com>
2022-08-17 22:49:22 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+const Home = () => {
+  return <h2>Home</h2>
+}
+const About = () => {
+  return <h2>About</h2>
+}
+const Testing = () => {
+  return <h2>Testing</h2>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/testing">Testing</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="content">
+            <Routes>
+              <Route path="/about" element={<About />} />
+              <Route path="/testing" element={<Testing />} />
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </div>
+
+        </div>
+      </Router>
+
     </div>
   );
 }
